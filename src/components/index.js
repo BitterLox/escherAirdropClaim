de5cac9056289608de1e528ca1f6340af5df27a4
Luke Williams <luke@iquidus.io>
2018-03-14 06:12:30 +0100

    remove unused components

--- src/components/index.js
+++ src/components/index.js
@@ -3,13 +3,11 @@ import Callout from './Callout.vue'
 import Footer from './Footer.vue'
 import Header from './Header.vue'
 import Sidebar from './Sidebar/Sidebar.vue'
-import Switch from './Switch.vue'
 
 export {
   Breadcrumb,
   Callout,
   Footer,
   Header,
-  Sidebar,
-  Switch
+  Sidebar
 }
