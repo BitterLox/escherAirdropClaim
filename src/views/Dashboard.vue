f28b39213f0f02fec1d6e650994264791d18486e
Luke Williams <luke@iquidus.io>
2018-03-25 06:17:06 +0200

    make timeline bubbles and status dynamic

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -66,36 +66,36 @@
             </b-row>
           </b-col>
           <div class="timeline">
-            <div class="timeline-container in-progress left">
-              <div class="timeline-content in-progress">
+            <div :class="{'timeline-container': true, 'in-progress': timeline['0'].active, 'pending': timeline['0'].pending, 'complete': timeline['0'].complete, 'left': true }">
+              <div :class="{'timeline-content': true, 'in-progress': timeline['0'].active, 'pending': timeline['0'].pending, 'complete': timeline['0'].complete }">
                 <h3>Block 423,300</h3>
                 <p>End of test claim period.</p>
                 <p>No Escher will be airdropped, the test claim is to allow users to familarize themselves with the claim process.</p>
-                <strong>Status: in progress</strong>
+                <strong>Status: {{ timeline['0'].status }}</strong>
               </div>
             </div>
-            <div class="timeline-container in-progress right">
-              <div class="timeline-content in-progress">
+            <div :class="{'timeline-container': true, 'in-progress': timeline['1'].active, 'pending': timeline['1'].pending, 'complete': timeline['1'].complete, 'right': true }">
+              <div :class="{'timeline-content': true, 'in-progress': timeline['1'].active, 'pending': timeline['1'].pending, 'complete': timeline['1'].complete }">
                 <h3>Block 448,000</h3>
                 <p>End of Escher airdrop claim period #1, estimated to be 25th of April.</p>
                 <p>Escher will be airdropped at a 12ESCH per 1UBQ ratio to all participants.</p>
-                <strong>Status: in progress</strong>
+                <strong>Status: {{ timeline['1'].status }}</strong>
               </div>
             </div>
-            <div class="timeline-container pending left">
-              <div class="timeline-content pending">
+            <div :class="{'timeline-container': true, 'in-progress': timeline['2'].active, 'pending': timeline['2'].pending, 'complete': timeline['2'].complete, 'left': true }">
+              <div :class="{'timeline-content': true, 'in-progress': timeline['2'].active, 'pending': timeline['2'].pending, 'complete': timeline['2'].complete }">
                 <h3>Block 536,800</h3>
                 <p>End of Escher airdrop claim period #2, estimated to take place in July.</p>
                 <p>Escher will be airdropped at a 12ESCH per 1UBQ ratio to all participants.</p>
-                <strong>Status: pending</strong>
+                <strong>Status: {{ timeline['2'].status }}</strong>
               </div>
             </div>
-            <div class="timeline-container pending right">
-              <div class="timeline-content pending">
+            <div :class="{'timeline-container': true, 'in-progress': timeline['3'].active, 'pending': timeline['3'].pending, 'complete': timeline['3'].complete, 'right': true }">
+              <div :class="{'timeline-content': true, 'in-progress': timeline['3'].active, 'pending': timeline['3'].pending, 'complete': timeline['3'].complete }">
                 <h3>Block 625,600</h3>
                 <p>End of Escher airdrop claim period #3, estimated to take place in October.</p>
                 <p>Escher will be airdropped at a 12ESCH per 1UBQ ratio to all participants.</p>
-                <strong>Status: pending</strong>
+                <strong>Status: {{ timeline['3'].status }}</strong>
               </div>
             </div>
             <b-col md="12">
@@ -103,12 +103,12 @@
                 <label class="btn btn-info timeline-label">2019</label>
               </b-row>
             </b-col>
-            <div class="timeline-container pending left">
-              <div class="timeline-content pending">
+            <div :class="{'timeline-container': true, 'in-progress': timeline['4'].active, 'pending': timeline['4'].pending, 'complete': timeline['4'].complete, 'left': true }">
+              <div :class="{'timeline-content': true, 'in-progress': timeline['4'].active, 'pending': timeline['4'].pending, 'complete': timeline['4'].complete }">
                 <h3>Block 714,400</h3>
                 <p>End of Escher airdrop claim period #4, estimated to take place in January.</p>
                 <p>Escher will be airdropped at a 12ESCH per 1UBQ ratio to all participants.</p>
-                <strong>Status: pending</strong>
+                <strong>Status: {{ timeline['4'].status }}</strong>
               </div>
             </div>
           </div>
@@ -127,7 +127,39 @@ export default {
     return {
       currentBlock: '-',
       totalClaims: '-',
-      errors: []
+      errors: [],
+      timeline: {
+        0: {
+          pending: false,
+          active: true,
+          complete: false,
+          status: 'in progress'
+        },
+        1: {
+          pending: false,
+          active: true,
+          complete: false,
+          status: 'in progress'
+        },
+        2: {
+          pending: true,
+          active: false,
+          complete: false,
+          status: 'pending'
+        },
+        3: {
+          pending: true,
+          active: false,
+          complete: false,
+          status: 'pending'
+        },
+        4: {
+          pending: true,
+          active: false,
+          complete: false,
+          status: 'pending'
+        }
+      }
     }
   },
   created () {
@@ -139,6 +171,61 @@ export default {
         .then(response => {
           this.currentBlock = response.data.block
           this.totalClaims = response.data.claims
+          if (this.currentBlock > 423300) {
+            this.timeline['0'].pending = false
+            this.timeline['0'].active = false
+            this.timeline['0'].complete = true
+            this.timeline['0'].status = 'complete'
+          }
+
+          if (this.currentBlock > 448000) {
+            this.timeline['1'].pending = false
+            this.timeline['1'].active = false
+            this.timeline['1'].complete = true
+            this.timeline['1'].status = 'complete'
+          }
+
+          if (this.currentBlock >= 448800) {
+            this.timeline['2'].pending = false
+            this.timeline['2'].active = true
+            this.timeline['2'].complete = false
+            this.timeline['2'].status = 'in progress'
+          }
+
+          if (this.currentBlock > 536800) {
+            this.timeline['2'].pending = false
+            this.timeline['2'].active = false
+            this.timeline['2'].complete = true
+            this.timeline['2'].status = 'complete'
+          }
+
+          if (this.currentBlock >= 537600) {
+            this.timeline['3'].pending = false
+            this.timeline['3'].active = true
+            this.timeline['3'].complete = false
+            this.timeline['3'].status = 'in progress'
+          }
+
+          if (this.currentBlock > 625600) {
+            this.timeline['3'].pending = false
+            this.timeline['3'].active = false
+            this.timeline['3'].complete = true
+            this.timeline['3'].status = 'complete'
+          }
+
+          if (this.currentBlock >= 626400) {
+            this.timeline['4'].pending = false
+            this.timeline['4'].active = true
+            this.timeline['4'].complete = false
+            this.timeline['4'].status = 'in progress'
+          }
+
+          if (this.currentBlock > 714400) {
+            this.timeline['4'].pending = false
+            this.timeline['4'].active = false
+            this.timeline['4'].complete = true
+            this.timeline['4'].status = 'complete'
+          }
         })
         .catch(e => {
           this.errors.push(e)
